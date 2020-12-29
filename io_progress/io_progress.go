e9e55f3cc36d6cb6d24c40e0e722394974721bcf
Ryo Ota <nwtgck@nwtgck.org>
2020-12-30 01:47:46 +0900

    display progress in every one second

--- io_progress/io_progress.go
+++ io_progress/io_progress.go
@@ -5,6 +5,7 @@ import (
 	"github.com/nwtgck/go-piping-tunnel/util"
 	"io"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -18,10 +19,31 @@ type IOProgress struct {
 	makeMessage     func(progress *IOProgress) string
 	maxMessageLen   int
 	lastDisplayTime time.Time
+	finDisplayCh    chan struct{}
+	isCloseCalled   bool
+	// To process .Close() once
+	closeMutex *sync.Mutex
 }
 
 func NewIOProgress(writer io.Writer, reader io.Reader, messageWriter io.Writer, makeMessage func(progress *IOProgress) string) *IOProgress {
-	return &IOProgress{reader: reader, writer: writer, messageWriter: messageWriter, StartTime: time.Now(), makeMessage: makeMessage}
+	p := &IOProgress{
+		reader:        reader,
+		writer:        writer,
+		messageWriter: messageWriter,
+		StartTime:     time.Now(),
+		makeMessage:   makeMessage,
+		finDisplayCh:  make(chan struct{}, 1),
+		closeMutex:    new(sync.Mutex),
+		isCloseCalled: false,
+	}
+	go func() {
+		// Loop for displaying progress
+		for len(p.finDisplayCh) == 0 {
+			p.displayProgress()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	return p
 }
 
 func (progress *IOProgress) Read(p []byte) (int, error) {
@@ -30,7 +52,6 @@ func (progress *IOProgress) Read(p []byte) (int, error) {
 		return n, err
 	}
 	progress.CurrReadBytes += uint64(n)
-	go progress.displayIfShould()
 	return n, nil
 }
 
@@ -40,11 +61,22 @@ func (progress *IOProgress) Write(p []byte) (int, error) {
 		return n, err
 	}
 	progress.CurrWriteBytes += uint64(n)
-	go progress.displayIfShould()
 	return n, nil
 }
 
 func (progress *IOProgress) Close() error {
+	// Lock to process once
+	progress.closeMutex.Lock()
+	// Unlock after processing
+	defer progress.closeMutex.Unlock()
+	// If already closed
+	if progress.isCloseCalled {
+		return nil
+	}
+	// Notify finish to display loop
+	progress.finDisplayCh <- struct{}{}
+	progress.isCloseCalled = true
+
 	var rErr error
 	var wErr error
 	if r, ok := progress.reader.(io.ReadCloser); ok {
@@ -56,10 +88,7 @@ func (progress *IOProgress) Close() error {
 	return util.CombineErrors(wErr, rErr)
 }
 
-func (progress *IOProgress) displayIfShould() {
-	if time.Since(progress.lastDisplayTime).Milliseconds() < 1000 {
-		return
-	}
+func (progress *IOProgress) displayProgress() {
 	// Make message
 	message := progress.makeMessage(progress)
 	// Clear & show message
