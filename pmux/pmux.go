22128f32641279b466080cf93702c666c414f856
Ryo Ota <nwtgck@nwtgck.org>
2021-03-04 09:01:21 +0900

    allow pmux client host to connect anytime when server host is running

--- pmux/pmux.go
+++ pmux/pmux.go
@@ -4,7 +4,6 @@ package pmux
 import (
 	"bytes"
 	"context"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
@@ -36,78 +35,20 @@ type client struct {
 	baseDownloadUrl string
 }
 
-type syncPacket struct {
+type syncJson struct {
 	SubPath string `json:"sub_path"`
 }
 
 const pmuxVersion uint32 = 1
 const pmuxMimeType = "application/pmux"
-const syncSubPath = "sync"
-const getTimeout = 50 * time.Second
+const syncSubPath = ""
+const httpTimeout = 50 * time.Second
 
 var IncompatiblePmuxVersion = errors.Errorf("incompatible pmux version, expected %d", pmuxVersion)
 var NonPmuxMimeTypeError = errors.Errorf("invalid content-type, expected %s", pmuxMimeType)
 
 func headersWithPmux(headers []piping_util.KeyValue) []piping_util.KeyValue {
 	return append(headers, piping_util.KeyValue{Key: "Content-Type", Value: pmuxMimeType})
-}
-
-func sendInitial(httpClient *http.Client, headers []piping_util.KeyValue, uploadUrl string) {
-	backoff := NewExponentialBackoff()
-	for {
-		versionBytes := make([]byte, 4)
-		binary.BigEndian.PutUint32(versionBytes, pmuxVersion)
-		res, err := piping_util.PipingSend(httpClient, headersWithPmux(headers), uploadUrl, bytes.NewReader(versionBytes))
-		if err != nil {
-			time.Sleep(backoff.NextDuration())
-			continue
-		}
-		if res.StatusCode != 200 {
-			time.Sleep(backoff.NextDuration())
-			continue
-		}
-		_, err = io.Copy(ioutil.Discard, res.Body)
-		if err != nil {
-			continue
-		}
-		break
-	}
-}
-
-func getInitial(httpClient *http.Client, headers []piping_util.KeyValue, downloadUrl string) error {
-	backoff := NewExponentialBackoff()
-	for {
-		ctx, cancel := context.WithTimeout(context.Background(), getTimeout)
-		defer cancel()
-		getRes, err := piping_util.PipingGetWithContext(ctx, httpClient, headers, downloadUrl)
-		if err != nil {
-			// If timeout
-			if e, ok := err.(net.Error); ok && e.Timeout() {
-				backoff.Reset()
-				continue
-			}
-			// backoff
-			time.Sleep(backoff.NextDuration())
-			continue
-		}
-		if getRes.StatusCode != 200 {
-			time.Sleep(backoff.NextDuration())
-			continue
-		}
-		contentType := getRes.Header.Get("Content-Type")
-		if contentType != pmuxMimeType {
-			return NonPmuxMimeTypeError
-		}
-		resBytes, err := ioutil.ReadAll(getRes.Body)
-		if err != nil {
-			continue
-		}
-		version := binary.BigEndian.Uint32(resBytes[0:4])
-		if version != pmuxVersion {
-			return IncompatiblePmuxVersion
-		}
-		return nil
-	}
 }
 
 func Server(httpClient *http.Client, headers []piping_util.KeyValue, baseUploadUrl string, baseDownloadUrl string) (*server, error) {
@@ -117,14 +58,7 @@ func Server(httpClient *http.Client, headers []piping_util.KeyValue, baseUploadU
 		baseUploadUrl:   baseUploadUrl,
 		baseDownloadUrl: baseDownloadUrl,
 	}
-	err := server.initialConnect()
-	return server, err
-}
-
-func (s *server) initialConnect() error {
-	err := getInitial(s.httpClient, s.headers, s.baseDownloadUrl)
-	sendInitial(s.httpClient, s.headers, s.baseUploadUrl)
-	return err
+	return server, nil
 }
 
 type getSubPathStatusError struct {
@@ -135,30 +69,29 @@ func (e *getSubPathStatusError) Error() string {
 	return fmt.Sprintf("not status 200, found: %d", e.statusCode)
 }
 
-func (s *server) getSubPath() (string, error) {
-	downloadUrl, err := util.UrlJoin(s.baseDownloadUrl, syncSubPath)
+func (s *server) sendSubPath() (string, error) {
+	uploadUrl, err := util.UrlJoin(s.baseUploadUrl, syncSubPath)
 	if err != nil {
 		return "", err
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), getTimeout)
+	// TODO: send pmux version too
+	subPath := strings.Replace(uuid.New().String(), "-", "", 4)
+	sync := syncJson{SubPath: subPath}
+	jsonBytes, err := json.Marshal(sync)
+	if err != nil {
+		return "", err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), httpTimeout)
 	defer cancel()
-	getRes, err := piping_util.PipingGetWithContext(ctx, s.httpClient, s.headers, downloadUrl)
+	postRes, err := piping_util.PipingSendWithContext(ctx, s.httpClient, headersWithPmux(s.headers), uploadUrl, bytes.NewReader(jsonBytes))
 	if err != nil {
 		return "", err
 	}
-	if getRes.StatusCode != 200 {
-		return "", &getSubPathStatusError{statusCode: getRes.StatusCode}
-	}
-	resBytes, err := ioutil.ReadAll(getRes.Body)
+	_, err = io.Copy(ioutil.Discard, postRes.Body)
 	if err != nil {
 		return "", err
 	}
-	var packet syncPacket
-	err = json.Unmarshal(resBytes, &packet)
-	if err != nil {
-		return "", err
-	}
-	return packet.SubPath, nil
+	return subPath, nil
 }
 
 func (s *server) Accept() (io.ReadWriteCloser, error) {
@@ -166,7 +99,7 @@ func (s *server) Accept() (io.ReadWriteCloser, error) {
 	var subPath string
 	for {
 		var err error
-		subPath, err = s.getSubPath()
+		subPath, err = s.sendSubPath()
 		if err == nil {
 			break
 		}
@@ -176,10 +109,6 @@ func (s *server) Accept() (io.ReadWriteCloser, error) {
 			backoff.Reset()
 			// No backoff
 			continue
-		}
-		// Skip error logging if status error
-		if _, ok := err.(*getSubPathStatusError); !ok {
-			fmt.Printf("get sync error: %+v\n", errors.WithStack(err))
 		}
 		// backoff
 		time.Sleep(backoff.NextDuration())
@@ -206,35 +135,34 @@ func Client(httpClient *http.Client, headers []piping_util.KeyValue, baseUploadU
 		baseUploadUrl:   baseUploadUrl,
 		baseDownloadUrl: baseDownloadUrl,
 	}
-	err := client.initialConnect()
-	return client, err
+	return client, nil
 }
 
-func (c *client) initialConnect() error {
-	sendInitial(c.httpClient, c.headers, c.baseUploadUrl)
-	return getInitial(c.httpClient, c.headers, c.baseDownloadUrl)
-}
-
-func (c *client) sendSubPath() (string, error) {
-	subPath := strings.Replace(uuid.New().String(), "-", "", 4)
-	packet := syncPacket{SubPath: subPath}
-	jsonBytes, err := json.Marshal(packet)
+func (c *client) getSubPath() (string, error) {
+	downloadUrl, err := util.UrlJoin(c.baseDownloadUrl, syncSubPath)
 	if err != nil {
 		return "", err
 	}
-	uploadUrl, err := util.UrlJoin(c.baseUploadUrl, syncSubPath)
-	if err != nil {
-		return "", err
-	}
-	res, err := piping_util.PipingSend(c.httpClient, c.headers, uploadUrl, bytes.NewReader(jsonBytes))
+	res, err := piping_util.PipingGet(c.httpClient, c.headers, downloadUrl)
 	if err != nil {
 		return "", err
 	}
 	if res.StatusCode != 200 {
-		return "", errors.Errorf("not status 200, found: %d", res.StatusCode)
+		return "", &getSubPathStatusError{statusCode: res.StatusCode}
 	}
-	_, err = io.Copy(ioutil.Discard, res.Body)
-	return subPath, err
+	if res.Header.Get("Content-Type") != pmuxMimeType {
+		return "", NonPmuxMimeTypeError
+	}
+	resBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	var sync syncJson
+	err = json.Unmarshal(resBytes, &sync)
+	if err != nil {
+		return "", err
+	}
+	return sync.SubPath, nil
 }
 
 func (c *client) Open() (io.ReadWriteCloser, error) {
@@ -242,11 +170,19 @@ func (c *client) Open() (io.ReadWriteCloser, error) {
 	var subPath string
 	for {
 		var err error
-		subPath, err = c.sendSubPath()
+		subPath, err = c.getSubPath()
 		if err == nil {
 			break
 		}
-		fmt.Fprintln(os.Stderr, "send sync error", err)
+		// If timeout
+		if e, ok := err.(net.Error); ok && e.Timeout() {
+			backoff.Reset()
+			continue
+		}
+		if err == NonPmuxMimeTypeError {
+			return nil, err
+		}
+		fmt.Fprintln(os.Stderr, "get sync error", err)
 		time.Sleep(backoff.NextDuration())
 	}
 	uploadUrl, err := util.UrlJoin(c.baseUploadUrl, subPath)
